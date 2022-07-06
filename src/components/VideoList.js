ea2c1dceb97745cfc6728df62c4e5b2bd3e741b8
Ji Sung OH <kopiuy678@hotmail.com>
2022-07-06 23:17:50 +0900

    copied

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,40 +1,18 @@
 import React from "react";
+import VideoItem from "./VideoItem";
 
-const VideoList = (props) => {
-  const HTMLRender = props.videos.map((video) => {
-    const { id } = video;
+const VideoList = ({ videos, onVideoSelect }) => {
+  const renderedList = videos.map((video) => {
     return (
-      <div className="ui card">
-        <div className="content">
-          <div className="right floated meta">14h</div>
-          <video
-            className="ui avatar image"
-            src={`https://www.youtube.com/watch?v=${id.videoId}`}
-          />{" "}
-          Elliot
-        </div>
-        <div className="image"></div>
-        <div className="content">
-          <span className="right floated">
-            <i className="heart outline like icon"></i>
-            17 likes
-          </span>
-          <i className="comment icon"></i>3 comments
-        </div>
-        <div className="extra content">
-          <div className="ui large transparent left icon input">
-            <i className="heart outline icon"></i>
-            <input type="text" placeholder="Add Comment..." />
-          </div>
-        </div>
-      </div>
+      <VideoItem
+        key={video.id.videoId}
+        onVideoSelect={onVideoSelect}
+        video={video}
+      />
     );
   });
 
-  // const HTMLRender = "dd";
-  // console.log(props.videos);
-
-  return <div className="ui segment">{HTMLRender}</div>;
+  return <div className="ui relaxed divided list">{renderedList}</div>;
 };
 
 export default VideoList;
--- src/pages/Video.js
+++ src/pages/Video.js
@@ -2,6 +2,7 @@ import React from "react";
 import SearchBar from "../components/SearchBar";
 import getVideo from "../api/getVideo";
 import VideoList from "../components/VideoList";
+import VideoDetail from "../components/VideoDetail";
 
 class Video extends React.Component {
   // state = { images: [] };
@@ -16,7 +17,12 @@ class Video extends React.Component {
     super(props);
     this.state = {
       videos: [],
+      selectedVideo: null,
     };
+  }
+
+  componentDidMount() {
+    this.onTermSubmit("GLC");
   }
 
   onTermSubmit = async (term) => {
@@ -26,16 +32,35 @@ class Video extends React.Component {
       },
     });
 
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
   };
+
+  onVideoSelected(video) {
+    this.setState({ selectedVideo: video });
+  }
   render() {
     return (
-      <div>
-        <h1>Video Page</h1>
-        <SearchBar onFormSubmit={this.onTermSubmit} />I have
-        {this.state.videos.length} videos.
-        <VideoList videos={this.state.videos} />
-      </div>
+      <>
+        <div className="ui container">
+          <SearchBar onFormSubmit={this.onTermSubmit} />
+          <div className="ui grid">
+            <div className="ui row">
+              <div className="eleven wide column">
+                <VideoDetail video={this.state.selectedVideo} />
+              </div>
+              <div className="five wide column">
+                <VideoList
+                  onVideoSelect={this.onVideoSelect}
+                  videos={this.state.videos}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
     );
   }
 }
